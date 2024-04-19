c84b7000ceff87763cea2bdac2c350c84043be57
jesus <jesus@lt-1255>
2024-04-19 23:02:26 +0200

    one more fc layer

--- car_racing_continous/neural_net.py
+++ car_racing_continous/neural_net.py
@@ -8,10 +8,11 @@ class ActorCriticCNN(nn.Module):
     def __init__(self, n_outputs, std=0.0):
         super().__init__()
 
-        self.crit_conv1 = nn.Conv2d(3, 16, 3)
+        self.crit_conv1 = nn.Conv2d(2, 16, 3)
         self.crit_conv2 = nn.Conv2d(16, 8, 3)
-        self.crit_fc1 = nn.Linear(392, 1)
-        self.act_fc1 = nn.Linear(392, n_outputs)
+        self.crit_fc1 = nn.Linear(512, 128)
+        self.crit_fc2 = nn.Linear(128, 1)
+        self.act_fc1 = nn.Linear(128, n_outputs)
 
         self.log_std = nn.Parameter(torch.ones(n_outputs) * std)
 
@@ -20,8 +21,9 @@ class ActorCriticCNN(nn.Module):
         x = F.max_pool2d(F.leaky_relu(self.crit_conv1(x)), 2, 2)
         x = F.max_pool2d(F.leaky_relu(self.crit_conv2(x)), 2, 2)
         x = torch.flatten(x, 1)
+        x = F.leaky_relu(self.crit_fc1(x))
 
-        value = F.leaky_relu(self.crit_fc1(x))
+        value = F.leaky_relu(self.crit_fc2(x))
         mu = F.leaky_relu(self.act_fc1(x))
 
         std   = self.log_std.exp().diag()
